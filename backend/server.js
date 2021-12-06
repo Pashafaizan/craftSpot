b095e8653310f13a95aaebe586bb5e07ef2b4808
Pashafaizan <faizanpasha10044@gmail.com>
2021-12-06 13:35:22 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,15 @@ app.use(express.static("./uploads"));
 
 app.use("/images/uploads", express.static("./uploads"));
 
-
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
 app.use(express.json());
 
 app.use("/api/v1", routes);
 // app.use(api/v1/routes)
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(7860, () => {
   console.log("your server has been started");
